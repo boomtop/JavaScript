6dbbcaab96aa2a8a8ff8e76c137125503c9515f0
Alexeev Victor wrk <avlipro@outlook.com>
2019-06-04 13:10:33 +0300

    learn JS
    Signed-off-by: Alexeev Victor wrk <avlipro@outlook.com>

---
+++ test.js
@@ -0,0 +1,36 @@
+'use strict' //ES6 стандарт (нужно ставить) IE9- не поддерживает
+
+
+// person - объкт
+let person = {
+    name: "Vic",
+    age: 19,
+    logAge: true
+};
+console.log(person.age);
+console.log(person ['age']);
+
+let arr = ["one.png", "two.png", "three.png"]; //массив
+console.log(arr[0]);
+
+
+//модальное окно с ОК и Отмена
+// let answer = confirm("Hi"); 
+// console.log(answer);
+
+// let answer = prompt('Любой текст', 'Текст по умолчанию'); 
+// console.log(answer);
+// console.log(typeof(answer));
+
+// "+" переводит тип строка в тип число
+// let answer = +prompt('Любой текст', 'Текст по умолчанию'); 
+// console.log(answer);
+// console.log(typeof(answer));
+
+// let ink = 10, dek = 10;
+// console.log(++ink);
+// console.log(--dek);
+// console.log(ink++); // сначала выводит потом применяет действие
+// console.log(dek--);
+
+
