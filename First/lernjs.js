069b43fbf1771d8e0203bff678b81d1664100d30
Alexeev Victor wrk <avlipro@outlook.com>
2019-06-11 16:19:02 +0300

    Добавил задачу

--- First/lernjs.js
+++ First/lernjs.js
@@ -1,18 +1,21 @@
 "use strict";
 
-function chekAge(age){
-    if (age>18) {
-        return true;
-    } else {
-        return confirm("What???");
+
+// //3 функции реализуют одно и тоже
+// function chekAge(age){
+//     if (age>18) {
+//         return true;
+//     } else {
+//         return confirm("What???");
     
-    }
-}
+//     }
+// }
 
-function checkAge2(age){
-    return (age>18) ? true : confirm("What?");
-}
+// function checkAge2(age){
+//     return (age>18) ? true : confirm("What?");
+// }
 
-function checkAge3(age) {
-    return (age>18) || confirm("What?");
-}
+// function checkAge3(age) {
+//     return (age>18) || confirm("What?");
+// }
+
