2201ed115f523a8cd0754ad291fd9b00862856a9
Alexeev Victor wrk <avlipro@outlook.com>
2020-08-04 13:36:51 +0300

    04/08/2020 Второй подход

---
+++ JSudemy/script.js
@@ -0,0 +1,16 @@
+// Тип данных объект
+
+// let person = {
+//     name: 'Victor',
+//     number: 25552
+
+// };
+
+// console.log(person.name);
+// console.log(person ["name"]);
+
+// //Массивы
+// let arr = ['log.txt', 'foto.png'];
+// console.log(arr[0]);
+
+let num = 50;
